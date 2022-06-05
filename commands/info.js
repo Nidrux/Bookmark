9e9892f4f91047ae462e188849bc3c88158d83dc
Nidrux <37250273+Nidrux@users.noreply.github.com>
2022-06-05 15:20:21 +0200

    info command

---
+++ commands/info.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const logger = require("../modules/logger");
+const os = require("os");
+const getStatistics = require("../modules/getStatistics");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("info")
+    .setDescription('Get information about the bot and the system')
+    .setDefaultPermission(false),
+  async execute(interaction, client) {
+    if(interaction.user.id !== "881032853234806784") return logger.warning(`user:${interaction.user.username}:${interaction.user.id} used command /info`)
+    logger.info(`user:${interaction.user.username}:${interaction.user.id} used command /info`)
+    let stats = await getStatistics(client);
+    const embed = new MessageEmbed()  
+    .setColor("#ffae01")
+    .setTitle("🔖 Bookmark info")
+    .setDescription(`This message showcases the info embed for the bot Bookmark`)
+    .setThumbnail("https://cdn.discordapp.com/attachments/973562599792865330/973562706315604028/logo.png")
+    .addFields(
+        [
+            {name: "OS", value: `platform: ${os.platform} memory: ${~~(os.totalmem / 1024 / 1024)}MB`},
+            {name: "Statistics", value: stats}
+        ]
+    )
+    .setTimestamp();
+
+    await interaction.reply({
+      embeds: [embed],
+      ephemeral: true 
+    });
+  },
+};
--- modules/logger.js
+++ modules/logger.js
@@ -3,12 +3,11 @@ const logger = {
         console.log(`\u001b[44;1m\u001b[37mINFO:\u001b[0m ${string}`);
     },
     warning(string) {
-        console.log(`\u001b[43;1m\u001b[30mWARN:\u001b[0m ${string}`);
+        console.log(`\u001b[37m\u001b[45;1mWARN:\u001b[0m ${string}`);
     },
     error(string) {
         console.log(`\u001b[41;1m\u001b[37mERR:\u001b[0m ${string}`);
     }
 
 }
-
 module.exports = logger;
